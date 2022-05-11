21c9b57edb4f3e55d5f02a241599206f02fb17f9
Alessandro Brun <univale9966aa@gmail.com>
2022-05-11 15:39:16 +0200

    Initial commit

---
+++ regenerate/src/main/java/com/synclab/regenerate/RegenerateBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.synclab.regenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegenerateBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegenerateBackEndApplication.class, args);
+	}
+
+}
---
+++ regenerate/src/test/java/com/synclab/regenerate/RegenerateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synclab.regenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegenerateBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
