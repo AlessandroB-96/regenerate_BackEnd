29840dc5cbdb9bc567d93f8b1eb950284c2ffbd0
Alessandro Brun <univale9966aa@gmail.com>
2022-05-11 17:11:53 +0200

    Added Repositories

---
+++ regenerate/src/main/java/com/synclab/regenerate/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.synclab.regenerate.repository;
+
+import com.synclab.regenerate.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+}
---
+++ regenerate/src/main/java/com/synclab/regenerate/repository/VisitRepository.java
@@ -0,0 +1,11 @@
+package com.synclab.regenerate.repository;
+
+
+import com.synclab.regenerate.model.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+}
