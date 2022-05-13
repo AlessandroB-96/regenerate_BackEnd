16f1b3fe733257be98932297658e6069718879d8
Alessandro <univale9966aa@gmail.com>
2022-05-13 16:06:44 +0200

    Fixed DepartmentService methods

--- regenerate/src/main/java/com/synclab/regenerate/repository/DepartmentRepository.java
+++ regenerate/src/main/java/com/synclab/regenerate/repository/DepartmentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    Department findDepartmentById(Long id);
+    Department findDepartmentByIdDepartment(Long id);
 }
--- regenerate/src/main/java/com/synclab/regenerate/service/DepartmentService.java
+++ regenerate/src/main/java/com/synclab/regenerate/service/DepartmentService.java
@@ -5,15 +5,22 @@ import com.synclab.regenerate.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DepartmentService {
 
     @Autowired
     private DepartmentRepository departmentRepository;
 
-    //Method that
+    //Method that returns all the departments
+    public List<Department> findAllDepartment (){
+        return departmentRepository.findAll();
+    }
+
+    //Method that return a department name based on id
     public String findDepartmentById ( Long id ){
-        Department department = departmentRepository.findDepartmentById(id);
+        Department department = departmentRepository.findDepartmentByIdDepartment(id);
         return department.getName();
     }
 }
