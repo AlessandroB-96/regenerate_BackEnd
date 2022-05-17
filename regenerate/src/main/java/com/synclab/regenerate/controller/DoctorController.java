08db66067984e7d718b5bd94151485008b7e4bf6
Alessandro <univale9966aa@gmail.com>
2022-05-17 17:30:39 +0200

    Fixed DoctorController getAllDoctors method

--- regenerate/src/main/java/com/synclab/regenerate/controller/DoctorController.java
+++ regenerate/src/main/java/com/synclab/regenerate/controller/DoctorController.java
@@ -9,10 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/doctor")
 public class DoctorController {
 
     @Autowired
