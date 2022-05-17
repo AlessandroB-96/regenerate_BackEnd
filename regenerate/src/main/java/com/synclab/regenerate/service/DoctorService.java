61188ac0fa395ed1c4bb21683354e53e7189e617
Daniele <daniele96mancini@gmail.com>
2022-05-17 14:27:43 +0200

    Created Doctor Methods

--- regenerate/src/main/java/com/synclab/regenerate/service/DoctorService.java
+++ regenerate/src/main/java/com/synclab/regenerate/service/DoctorService.java
@@ -1,12 +1,25 @@
 package com.synclab.regenerate.service;
 
+import com.synclab.regenerate.model.Department;
+import com.synclab.regenerate.model.Doctor;
 import com.synclab.regenerate.repository.CustomerRepository;
+import com.synclab.regenerate.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoctorService {
 
     @Autowired
-    private CustomerRepository customerRepository;
+    private DoctorRepository doctorRepositery;
+
+    public List<Doctor> getAllDoctors(){
+        return doctorRepositery.findAll();
+    }
+
+    public List<Doctor> findDoctorsByIdDepartment(Department id) {
+        return doctorRepositery.findDoctorByIdDepartment(id);
+    }
 }
