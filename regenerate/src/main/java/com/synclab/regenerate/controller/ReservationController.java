9420372b2d32c4ab503bbd8ce4e19fd751359129
Alessandro Brun <univale9966aa@gmail.com>
2022-05-23 15:59:06 +0200

    Removed unused imports

--- regenerate/src/main/java/com/synclab/regenerate/controller/ReservationController.java
+++ regenerate/src/main/java/com/synclab/regenerate/controller/ReservationController.java
@@ -2,13 +2,11 @@ package com.synclab.regenerate.controller;
 
 import com.synclab.regenerate.model.Reservation;
 import com.synclab.regenerate.service.ReservationService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
