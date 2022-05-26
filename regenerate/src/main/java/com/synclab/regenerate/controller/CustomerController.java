b6be54377d3526dde84a97abe981e2bf80cf5ce7
Alessandro <univale9966aa@gmail.com>
2022-05-26 14:34:07 +0200

    Added method getCustomers

--- regenerate/src/main/java/com/synclab/regenerate/controller/CustomerController.java
+++ regenerate/src/main/java/com/synclab/regenerate/controller/CustomerController.java
@@ -1,12 +1,27 @@
 package com.synclab.regenerate.controller;
 
+import com.synclab.regenerate.model.Customer;
 import com.synclab.regenerate.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Customer>> getCustomer (){
+        List<Customer> customers = customerService.getCustomers();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
 }
--- regenerate/src/main/java/com/synclab/regenerate/service/CustomerService.java
+++ regenerate/src/main/java/com/synclab/regenerate/service/CustomerService.java
@@ -1,12 +1,20 @@
 package com.synclab.regenerate.service;
 
+import com.synclab.regenerate.model.Customer;
 import com.synclab.regenerate.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    public List<Customer> getCustomers () {
+        return customerRepository.findAll();
+    }
+
 }
